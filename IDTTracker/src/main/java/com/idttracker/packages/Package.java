eda94c7dcc651d12c7d0131058221af861ea7f15
ben3571 <beng3571@gmail.com>
2016-02-14 14:50:33 -0500

    added ETA calculations

--- IDTTracker/src/main/java/com/idttracker/packages/Package.java
+++ IDTTracker/src/main/java/com/idttracker/packages/Package.java
@@ -10,8 +10,9 @@ import java.sql.Timestamp;
 *
 */
 public class Package {
-	private double desLat, desLon, curLat, curLon, dist;
+	private double desLat, desLon, curLat, curLon, dist, totalDist, setDist;
 	private String name;
+	public Timestamp first, last, ETA;
 
 	public Package(double destinationLat, double destinationLon, String n){
 		name = n;
@@ -19,7 +20,7 @@ public class Package {
 		desLon = destinationLon;
 
 	}
-	public void update(double lat, double lon,Timestamp time){
+	public void update(double lat, double lon, Timestamp time){
 		
 		curLat = lat;
 		curLon = lon;
@@ -28,7 +29,21 @@ public class Package {
 		dist = Math.acos(dist);
 		dist = Math.toDegrees(dist);
 		dist = dist * 60 * 1.1515;
-		
+		first = last;
+		if(first==null){
+			last = time;
+			totalDist = dist;
+		}
+		else{
+			last = time;
+			long firstInMillis = first.getTime();
+			long lastInMillis = last.getTime();
+			long difference = lastInMillis - firstInMillis;
+			setDist = totalDist - dist;
+			long average = (long) (difference/setDist);
+			long distLeft = (long) (dist*average)+lastInMillis;
+			ETA = new Timestamp(distLeft);
+		}
 	}
 	public double[] getLocation(){
 		return new double [] {curLat,curLon};
@@ -44,7 +59,7 @@ public class Package {
 		return dist;
 	}
 	public Timestamp getETA(){
-		return null;
+		return ETA;
 	}
 	public String toString(){
 		return "Name: " + name + "ETA: " + null + " Distance: " + dist + "CurLat/lon:" + curLat+"/"+curLon + "DesLat/Lon: " + desLat+"/"+desLon;
